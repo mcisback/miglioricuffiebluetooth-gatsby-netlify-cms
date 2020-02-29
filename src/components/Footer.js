c2e7b4c58f3ebc898478ecc6db77a46cc1d286a3
Marco Caggiano <mcisback@gmail.com>
2020-02-29 04:36:57 +0100

    +logo footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/logo.svg'
+import logoWhite from '../img/logo-white.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
@@ -13,8 +13,8 @@ const Footer = class extends React.Component {
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
           <img
-            src={logo}
-            alt="Kaldi"
+            src={logoWhite}
+            alt="Migliori Cuffie Bluetooth"
             style={{ width: '14em', height: '10em' }}
           />
         </div>
